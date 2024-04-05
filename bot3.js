838e37f294f7ef3081790d21898f8254029fa8ab
SYH <85495636+SYH08@users.noreply.github.com>
2024-04-05 15:47:19 +0900

    ?

--- bot3.js
+++ bot3.js
@@ -9,10 +9,7 @@ function response(room, msg, sender, isGroupChat, replier, imageDB, packageName)
 	} else if (msg == '!홍기석') {
         replier.reply("대가리 몽키~");
     }
-=======
     if (msg == '!ping') {
         replier.reply("!pong "+room);
     }
-
->>>>>>> a050cca26966f9ca3ace6526d3683c940f851453
 }
