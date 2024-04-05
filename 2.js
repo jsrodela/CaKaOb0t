4d8e937d4d18dfc423b5e048228afd6e91cbf63f
Seohyun Park <bonopark08@gmail.com>
2024-04-05 15:38:46 +0900

    !박서현하면 대답

---
+++ 2.js
@@ -0,0 +1,7 @@
+const scriptName = "2";
+
+function response(room, msg, sender, isGroupChat, replier, imageDB, packageName) {
+    if (msg == '!박서현') {
+        replier.reply("!Hello"+room);
+    }
+}
