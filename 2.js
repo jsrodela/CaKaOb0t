181b4decac8884e04a78db7db430a77fe5693ae9
Seohyun Park <bonopark08@gmail.com>
2024-04-05 15:57:34 +0900

    메롱 메롱

--- 2.js
+++ 2.js
@@ -4,4 +4,7 @@ function response(room, msg, sender, isGroupChat, replier, imageDB, packageName)
     if (msg == '!박서현') {
         replier.reply("!Hello"+room);
     }
+    if (msg == '메롱') {
+        replier.reply("메롱"+room);
+    }
 }
